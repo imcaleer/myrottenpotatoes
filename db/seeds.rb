6b99a5fd1c105baccbd85a89ae9b24292a379ebb
Ian McAleer <imcaleer@hotmail.com>
2017-10-06 13:40:58 +0100

    Commit before moving

---
+++ db/seeds.rb
@@ -0,0 +1,13 @@
+# Seed the RottenPotatoes DB with some movies.
+more_movies = [
+    {:title => 'Aladdin', :rating => 'G', :release_date => '25-Nov-1992'},
+    {:title => 'When Harry Met Sally', :rating => 'R', :release_date => '21-Jul-1989'},
+    {:title => 'The Help', :rating => 'PG-13', :release_date => '10-Aug-2011'},
+    {:title => 'Raiders of the Lost Ark', :rating => 'PG', :release_date => '12-Jun-1981'},
+    {:title => 'Star Wars', :rating => 'PG', :release_date => '25-Apr-1977'},
+    {:title => 'Requiem for a dream', :rating => 'R', :release_date => '27-Oct-2000'}
+  ]
+  
+  more_movies.each do |movie|
+    Movie.create!(movie)
+  end
